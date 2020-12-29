ee384554e4a1af75c9238fe2847ac0ac16f0cd60
Chase Starr <chasestarr@gmail.com>
2020-12-29 19:16:00 +0000

    create tmp dir if needed

--- src/debug.rs
+++ src/debug.rs
@@ -1,17 +1,28 @@
 use image::{GrayImage, RgbImage};
 use std::env;
+use std::fs;
 use std::path::Path;
 
 pub fn write_gray(i: &GrayImage, name: &str) {
   if env::var("DEBUG").is_ok() {
-    let path = Path::new("./tmp").join(format!("{}.png", name));
+    let output_dir = Path::new("./tmp");
+    if !output_dir.is_dir() {
+      fs::create_dir(output_dir).expect("Failed to create output directory")
+    }
+
+    let path = output_dir.join(format!("{}.png", name));
     i.save(path).unwrap();
   }
 }
 
 pub fn write_rgb(i: &RgbImage, name: &str) {
   if env::var("DEBUG").is_ok() {
-    let path = Path::new("./tmp").join(format!("{}.png", name));
+    let output_dir = Path::new("./tmp");
+    if !output_dir.is_dir() {
+      fs::create_dir(output_dir).expect("Failed to create output directory")
+    }
+
+    let path = output_dir.join(format!("{}.png", name));
     i.save(path).unwrap();
   }
 }
